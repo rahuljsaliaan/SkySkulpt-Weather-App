7e5b2b037065bb9cb14fa4c542ead2542881f1b6
rahuljsaliaan <rahuljsaliaan@gmail.com>
2023-09-08 18:02:08 +0530

    fixed logo not displaying issue

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,12 @@
+import logo from "../assets/skyskulpt-logo.png";
+
 function Logo() {
   return (
     <a
       href="/"
       className="bg-black/50  px-4 py-2 rounded-full shadow-xl flex gap-3 items-center hover:bg-black/30"
     >
-      <img className="w-12" alt="logo" src="../assets/skyskulpt-logo.png" />
+      <img className="w-12" alt="logo" src={logo} />
       <h1 className="text-2xl text-white font-bold">SkySkulpt</h1>
     </a>
   );
