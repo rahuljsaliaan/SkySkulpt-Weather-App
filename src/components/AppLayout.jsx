97be12b87e431fa677172bdd3e5f1e1005fadaec
V Jeevan Kumar <50071187+naveej@users.noreply.github.com>
2023-09-08 18:47:23 +0530

    Update AppLayout.jsx

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -12,10 +12,10 @@ function AppLayout() {
   if (initialLoading) return <Loading />;
 
   return (
-    <div className="pt-20 px-6 min-h-screen bg-gradient-to-b from-indigo-900 via-blue-900 to-blue-800 h-full">
+    <div className="pt-20 px-6 min-h-screen bg-gradient-to-b from-indigo-900 via-blue-900 to-blue-800">
       {error && <ToastMessage type="error" message={error} />}
       <Navigation />
-      <div className="pt-5 flex flex-col justify-evenly px-20">
+      <div className="pt-5 flex flex-col justify-evenly px-20 h-full">
         <Title />
         <SelectedWeather />
         <WeatherForecast />
